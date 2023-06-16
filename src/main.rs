cf34684a55bff7f43b884533164dba30bd898d0e
DarioTD <dario.tabares@hotmail.com>
2023-06-15 20:18:07 -0500

    working xor sigmoid

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,151 @@
 use rand::Rng;
 
-const TRAINING_SET: [(f64, f64, f64); 4] = [
-    (0.0, 0.0, 0.0),
-    (1.0, 0.0, 1.0),
-    (0.0, 1.0, 1.0),
-    (1.0, 1.0, 1.0),
-];
+#[allow(dead_code)]
+#[derive(Debug)]
+enum Act {
+    None,
+    Sigmoid,
+    Sine,
+    Tanh,
+}
 
-fn cost(w1: f64, w2: f64) -> f64 {
-    let mut result = 0.0;
-    for i in 0..TRAINING_SET.len() {
-        let x1 = TRAINING_SET[i].0;
-        let x2 = TRAINING_SET[i].1;
-        let y = x1 * w1 + x2 * w2;
-        let d = y - TRAINING_SET[i].2;
-        result += f64::abs(d * d * d);
+impl Act {
+    fn activate(&self, x: f64) -> f64 {
+        match self {
+            Act::None => x,
+            Act::Sigmoid => Act::sigmoid(x),
+            Act::Sine => x.sin(),
+            Act::Tanh => x.tanh(),
+        }
     }
-    result /= TRAINING_SET.len() as f64;
-    result
+
+    fn sigmoid(x: f64) -> f64 {
+        1.0 / (1.0 + f64::exp(-x))
+    }
+}
+
+#[derive(Debug)]
+struct Neuron2in {
+    w1: f64,
+    w2: f64,
+    b: f64,
+}
+
+#[derive(Debug)]
+struct Xor {
+    n: [Neuron2in; 3],
+}
+
+impl Xor {
+    fn new() -> Self {
+        let n = [
+            Neuron2in {
+                w1: rand::thread_rng().gen_range(0.0..=1.0),
+                w2: rand::thread_rng().gen_range(0.0..=1.0),
+                b: rand::thread_rng().gen_range(0.0..=1.0),
+            },
+            Neuron2in {
+                w1: rand::thread_rng().gen_range(0.0..=1.0),
+                w2: rand::thread_rng().gen_range(0.0..=1.0),
+                b: rand::thread_rng().gen_range(0.0..=1.0),
+            },
+            Neuron2in {
+                w1: rand::thread_rng().gen_range(0.0..=1.0),
+                w2: rand::thread_rng().gen_range(0.0..=1.0),
+                b: rand::thread_rng().gen_range(0.0..=1.0),
+            },
+        ];
+        Xor { n }
+    }
+}
+
+const _XOR_TRAINING_SET: [[f64; 3]; 4] = [
+    [0.0, 0.0, 0.0],
+    [1.0, 0.0, 1.0],
+    [0.0, 1.0, 1.0],
+    [1.0, 1.0, 0.0],
+];
+const _OR_TRAINING_SET: [[f64; 3]; 4] = [
+    [0.0, 0.0, 0.0],
+    [1.0, 0.0, 1.0],
+    [0.0, 1.0, 1.0],
+    [1.0, 1.0, 1.0],
+];
+const _AND_TRAINING_SET: [[f64; 3]; 4] = [
+    [0.0, 0.0, 0.0],
+    [1.0, 0.0, 0.0],
+    [0.0, 1.0, 0.0],
+    [1.0, 1.0, 1.0],
+];
+const _NAND_TRAINING_SET: [[f64; 3]; 4] = [
+    [0.0, 0.0, 1.0],
+    [1.0, 0.0, 1.0],
+    [0.0, 1.0, 1.0],
+    [1.0, 1.0, 0.0],
+];
+const TRAINING_SET: &[[f64; 3]; 4] = &_XOR_TRAINING_SET;
+const ATC_M: Act = Act::Sigmoid;
+
+fn forwarding(m: &Xor, x: f64, y: f64) -> f64 {
+    let a = ATC_M.activate(x * m.n[0].w1 + y * m.n[0].w2 + m.n[0].b);
+    let b = ATC_M.activate(x * m.n[1].w1 + y * m.n[1].w2 + m.n[1].b);
+    ATC_M.activate(a * m.n[2].w1 + b * m.n[2].w2 + m.n[2].b)
+}
+
+fn cost(m: &Xor) -> f64 {
+    TRAINING_SET.iter().fold(0.0, |acc, i| {
+        let x1 = i[0];
+        let x2 = i[1];
+        let y = forwarding(&m, x1, x2);
+        let d = y - i[2];
+        acc + f64::abs(d * d * d)
+    }) / TRAINING_SET.len() as f64
 }
 
 fn main() {
-    let mut w1 = rand::thread_rng().gen_range(0.0..=10.0);
-    let mut w2 = rand::thread_rng().gen_range(0.0..=10.0);
-    let o_w1 = w1;
-    let o_w2 = w2;
+    let mut m = Xor::new();
+    let c = cost(&mut m);
+    println!("Original = m: {:#?}", m);
+    println!("cost: {c}");
+    println!();
     let rate = 1e-2;
-    let eps = 1e-2;
+    let eps = 1e-3;
 
-    for i in 0..10_000 {
-        let c = cost(w1, w2);
-        let d_w1 = (cost(w1 + eps, w2) - c) / w1;
-        let d_w2 = (cost(w1, w2 + eps) - c) / w2;
-        w1 -= rate * d_w1;
-        w2 -= rate * d_w2;
-        println!("{i} = w1: {}, w2: {}, c: {}", w1, w2, cost(w1, w2));
+    for i in 0..500_000 {
+        for j in 0..3 {
+            let c = cost(&m);
+
+            m.n[j].w1 += eps;
+            let d_w1 = (cost(&m) - c) / eps;
+            m.n[j].w1 -= eps;
+
+            m.n[j].w2 += eps;
+            let d_w2 = (cost(&m) - c) / eps;
+            m.n[j].w2 -= eps;
+
+            m.n[j].b += eps;
+            let d_b = (cost(&m) - c) / eps;
+            m.n[j].b -= eps;
+
+            m.n[j].w1 -= rate * d_w1;
+            m.n[j].w2 -= rate * d_w2;
+            m.n[j].b -= rate * d_b;
+        }
+        println!("{i} = m: {:#?}", m);
+        println!("cost: {}", cost(&m));
+        println!();
     }
     println!("-------------------------------");
+    println!("activation method: {:?}", ATC_M);
     println!();
-    println!("Original = w1: {}, w2: {}", o_w1, o_w2);
-    println!("Final = w1: {}, w2: {}, c: {}", w1, w2, cost(w1, w2));
+    println!("Final = m: {:#?}", m);
+    println!("cost: {}", cost(&m));
     println!();
-    for i in 0..TRAINING_SET.len() {
-        let x1 = TRAINING_SET[i].0;
-        let x2 = TRAINING_SET[i].1;
-        let ex_out = TRAINING_SET[i].2;
-        let y = x1 * w1 + x2 * w2;
+    for i in TRAINING_SET {
+        let x1 = i[0];
+        let x2 = i[1];
+        let ex_out = i[2];
+        let y = forwarding(&m, x1, x2);
         println!("x1: {}, x2: {}, ex_out: {}, y: {}", x1, x2, ex_out, y);
     }
 }
